6fcaf7ba8c2f3a9492deba7997118a6e512693c4
osbornroxas <osbornroxas02@gmail.com>
2020-09-28 02:32:37 -0700

    error fix

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -1,36 +1,44 @@
-//CRUD methods 
-// controller file to handle the Pizza model updates
-const { Pizza } = require('../models');
+// const { populate } = require('../models/Pizza');
 // const { params } = require('../routes');
+const { Pizza } = require('../models');
 
 const pizzaController = {
     // get all pizzas
     getAllPizza(req, res) {
         Pizza.find({})
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
             .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => {
                 console.log(err);
-                res.status(500).json(err);
+                res.status(400).json(err);
             });
     },
 
     // get one pizza by id
     getPizzaById({ params }, res) {
         Pizza.findOne({ _id: params.id })
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
             .then(dbPizzaData => {
-                // If no pizza is found, send 404
                 if (!dbPizzaData) {
                     res.status(404).json({ message: 'No pizza found with this id!' });
                     return;
                 }
-                res.json(dbPizzaData)
+                res.json(dbPizzaData);
             })
             .catch(err => {
                 console.log(err);
                 res.status(400).json(err);
             });
-        },
-
+    },
 
     // createPizza
     createPizza({ body }, res) {
--- models/Pizza.js
+++ models/Pizza.js
@@ -1,24 +1,48 @@
 // import the dependencies
+const moment = require('moment');
 const { Schema, model } = require('mongoose');
 
 // create the pizza schema
-const PizzaSchema = new Schema({
-    pizzaName: {
-        type: String
+const PizzaSchema = new Schema(
+    {
+        pizzaName: {
+            type: String
+        },
+        createdBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        },
+        size: {
+            type: String,
+            default: 'Large'
+        },
+        toppings: [],
+        comments: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Comment'
+            }
+        ]
     },
-    createdBy: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    size: {
-        type: String,
-        default: 'Large'
-    },
-    toppings: []
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+
+        id: false
+    }
+);
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function () {
+    return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
 });
+
 
 // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
